1b84fd6c411beb3adfa140aa14d17a40698263aa
risinggod <Frederik.paulsen@online.de>
2023-06-23 11:36:20 +0200

    added_the_necesery_GitHub_links

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from MainWindow import MainWindow
 import sys
 
 # All you need is https://doc.qt.io/qtforpython-6/
+
+# line edits, https://github.com/chey00/lineedits/ , https://doc.qt.io/qt-6/qlineedit.html#inputMask-prop
+
+# Login_fenster https://github.com/chey00/login/
+
+#Taschenrechner  https://github.com/chey00/calc/
+
+#drawing https://github.com/chey00/drawing/
+
+
+
+
 if __name__ == '__main__':
     application = QApplication(sys.argv)
 
